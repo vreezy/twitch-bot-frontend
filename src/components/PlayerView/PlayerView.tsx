7d9cbead4a0dded60ea1a7b93cdf5b4ff364a07c
vreezy117 <vreezy117@googlemail.com>
2020-05-09 13:38:25 +0200

    --- Auto-Git Commit ---

--- src/components/PlayerView/PlayerView.tsx
+++ src/components/PlayerView/PlayerView.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { ReactSVG } from 'react-svg'
 // import { useRef, useEffect,} from 'react';
 import player from '../../assets/player3.svg'; 
+import char from '../../assets/char.svg'; 
  
 import styles from './PlayerView.module.scss'; 
 
@@ -33,8 +34,22 @@ export function PlayerView(props: IPlayerView) {
                   all[1].textContent = "1"; 
                   all[2].textContent = props.wins.toString();
 
+
+                  var newElement = document.createElementNS("http://www.w3.org/2000/svg", 'image');
+                  newElement.setAttribute("x","15"); //Set path's data
+                  newElement.setAttribute("y","15");
+                  newElement.setAttribute("width","60%");
+                  newElement.setAttribute("height","60%");
+                  newElement.setAttribute("href",char);
+                  newElement.setAttribute("transform","translate(200,0) scale(-1, 1) translate(-100, -100) scale(2, 2) scale(1, 1) rotate(45, 100, 100)");
+
+
+                  // transform="scale(-2,2)"
+                  svg.appendChild(newElement);
+
                   svg.style.width = "100%";
                   svg.style.height = "100%";
+
                }
                // console.log(svg)
             }}
@@ -53,7 +68,43 @@ export function PlayerView(props: IPlayerView) {
             }}
          />
          <div className={styles.char}>
-            &nbsp;
+         {/* <ReactSVG
+            src={char}
+            afterInjection={(error, svg) => {
+               if (error) {
+                  console.error(error)
+                  return
+               }
+         
+               // if(svg !== undefined && svg !== null) {
+               //    // @ts-ignore
+               //    svg.querySelector("tspan").textContent = props.displayName;
+
+               //    const all = svg.querySelectorAll("tspan");
+               //    all[0].textContent = props.displayName;
+               //    // TODO: Calc Level based on xp
+               //    all[1].textContent = "1"; 
+               //    all[2].textContent = props.wins.toString();
+
+               //    svg.style.width = "100%";
+               //    svg.style.height = "100%";
+               // }
+               // console.log(svg)
+            }}
+            // beforeInjection={svg => {
+            //   svg.classList.add('svg-class-name')
+            //   svg.setAttribute('style', 'width: 200px')
+            // }}
+            evalScripts="always"
+            fallback={() => <span>Error!</span>}
+            loading={() => <span>Loading</span>}
+            renumerateIRIElements={false}
+            wrapper="span"
+            className={styles.player}
+            onClick={() => {
+               console.log('wrapper onClick')
+            }}
+         /> */}
          </div>
       </div>
    )
